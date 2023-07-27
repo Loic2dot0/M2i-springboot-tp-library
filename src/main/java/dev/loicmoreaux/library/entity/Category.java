5fc19036132d626287e38b4ae5338ed86e95dad7
Loic2dot0 <moreaux.loic@gmail.com>
2023-07-27 10:13:12 +0200

    update entity Category

--- src/main/java/dev/loicmoreaux/library/entity/Category.java
+++ src/main/java/dev/loicmoreaux/library/entity/Category.java
@@ -1,8 +1,49 @@
 package dev.loicmoreaux.library.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "categories")
 public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
 
+	/*
+	 * Constructors
+	 */
+	public Category() {}
+	
+	public Category(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Getters and Setters
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
 }
