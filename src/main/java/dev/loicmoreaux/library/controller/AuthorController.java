9a490dc22017221bc254c4e978b8638b66f3a813
Loic2dot0 <moreaux.loic@gmail.com>
2023-07-27 11:27:24 +0200

    update pom.xml, application.properties and categoryController. Create author controller

---
+++ src/main/java/dev/loicmoreaux/library/controller/AuthorController.java
@@ -0,0 +1,46 @@
+package dev.loicmoreaux.library.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.loicmoreaux.library.entity.Author;
+import dev.loicmoreaux.library.service.AuthorService;
+
+@RestController
+@RequestMapping("/authors")
+public class AuthorController {
+	
+	@Autowired
+	private AuthorService authorService;
+	
+	@GetMapping("/")
+	public ResponseEntity<List<Author>> getAllAuthors(){
+		return ResponseEntity.ok(authorService.getAllAuthors());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getAuthorById(@PathVariable("id") Integer id){
+		Optional<Author> optionalAuthor = authorService.getAuthorById(id);
+		
+		if(optionalAuthor.isPresent()) return ResponseEntity.ok(optionalAuthor.get());
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping("/")
+	public ResponseEntity<Author> createAuthor(@RequestBody Author author){
+		Author authorCreated = authorService.saveAuthor(author);
+		
+		return new ResponseEntity<Author>(authorCreated, HttpStatus.CREATED);
+	}
+}
