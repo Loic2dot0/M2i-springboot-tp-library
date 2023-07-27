3ddedd92ad5da6a3d108e259fbe876860c24a342
Loic2dot0 <moreaux.loic@gmail.com>
2023-07-27 13:43:42 +0200

    update controller author and category

--- src/main/java/dev/loicmoreaux/library/controller/AuthorController.java
+++ src/main/java/dev/loicmoreaux/library/controller/AuthorController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +44,28 @@ public class AuthorController {
 		Author authorCreated = authorService.saveAuthor(author);
 		
 		return new ResponseEntity<Author>(authorCreated, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> updateAuthor(@PathVariable("id") Integer id, @RequestBody Author modifiedAuthor){
+		if(id == null || id != modifiedAuthor.getId()) return ResponseEntity.badRequest().build();
+		
+		Optional<Author> optionalAuthor = authorService.getAuthorById(id);
+		
+		if(optionalAuthor.isPresent()) return ResponseEntity.ok(authorService.updateAuthor(modifiedAuthor));
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteAuthor(@PathVariable("id") Integer id){
+		Optional<Author> optionalAuthor = authorService.getAuthorById(id);
+		
+		if(optionalAuthor.isPresent()) {
+			authorService.deleteAuthor(id);
+			return ResponseEntity.ok().build();
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
--- src/main/java/dev/loicmoreaux/library/controller/CategoryController.java
+++ src/main/java/dev/loicmoreaux/library/controller/CategoryController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +44,28 @@ public class CategoryController {
 		Category categoryCreated = categoryService.saveCategory(category);
 		
 		return new ResponseEntity<Category>(categoryCreated, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> updateCategory(@PathVariable("id") Integer id, @RequestBody Category modifiedCategory){
+		if(id == null || id != modifiedCategory.getId()) return ResponseEntity.badRequest().build();
+		
+		Optional<Category> optionalCategory = categoryService.getCategoryById(id);
+		
+		if(optionalCategory.isPresent()) return ResponseEntity.ok(categoryService.updateCategory(modifiedCategory));
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteCategory(@PathVariable("id") Integer id){
+		Optional<Category> optionalCategory = categoryService.getCategoryById(id);
+		
+		if(optionalCategory.isPresent()) {
+			categoryService.deleteCategory(id);
+			return ResponseEntity.ok().build();
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
