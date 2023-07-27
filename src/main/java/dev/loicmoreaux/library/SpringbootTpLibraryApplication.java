2270cbe1f9fb78570761e1b92cb9972ba43d1559
Loic2dot0 <moreaux.loic@gmail.com>
2023-07-27 09:42:55 +0200

    first commit

---
+++ src/main/java/dev/loicmoreaux/library/SpringbootTpLibraryApplication.java
@@ -0,0 +1,13 @@
+package dev.loicmoreaux.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTpLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTpLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/loicmoreaux/library/SpringbootTpLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.loicmoreaux.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTpLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
